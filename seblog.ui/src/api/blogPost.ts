10293beaad68b4488bdf161da561ffe5748edb10
Sebastian <sebastian.johansson@hm.com>
2022-05-19 20:34:24 +0200

    add posting of blog to api

--- seblog.ui/src/api/blogPost.ts
+++ seblog.ui/src/api/blogPost.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { BlogPostData } from "../types/blogPostTypes";
+import { apiURL } from "./consts";
 
 export const postBlog = async (blogPost: BlogPostData) => {
-  return axios.post("/api/blogPost", blogPost);
+  return axios.post(apiURL, blogPost);
 };
