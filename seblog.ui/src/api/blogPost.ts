f0ba15582a56a2af16b51d3a6a7bf06d3120a829
Sebastian <seb.johansson@outlook.com>
2022-05-21 00:34:18 +0200

    update api urls consts

--- seblog.ui/src/api/blogPost.ts
+++ seblog.ui/src/api/blogPost.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { ApiBlogPost, BlogPostData } from "../types/blogPostTypes";
-import { apiURL } from "./consts";
+import { blogPostApiURL } from "./consts";
 
 export const postBlog = (blogPost: BlogPostData) => {
-  return axios.post(apiURL, blogPost);
+  return axios.post(blogPostApiURL, blogPost);
 };
 export const getAllBlogPosts = async (): Promise<ApiBlogPost[]> => {
-  return (await axios.get(apiURL)).data;
+  return (await axios.get(blogPostApiURL)).data;
 };
--- seblog.ui/src/api/consts.ts
+++ seblog.ui/src/api/consts.ts
@@ -1,1 +1,2 @@
-export const apiURL = "https://localhost:44371/api/Blog";
+export const blogPostApiURL = "https://localhost:44371/api/Blog";
+export const commentApiURL = "https://localhost:44371/api/Comments";
